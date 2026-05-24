efff886b7c575b89902ec66f74b4e8c266b1982b
25A31A05JY <sruthivadisetti7@gmail.com>
2026-05-24 15:43:32 +0530

    Add Navbar component for E-Shop

---
+++ Frontend/src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h2>E-Shop</h2>
+
+      <ul>
+        <li>Home</li>
+        <li>Products</li>
+        <li>Cart</li>
+        <li>Login</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
